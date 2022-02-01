51753b9d31feb5d3ea417c3d884cd7ece2b1c954
James Woolfenden <James.Woolfenden@gmail.com>
2022-02-01 09:53:25 +0000

    refresh

--- example/examplea/aws_cloudwatch_log_group.examplea.tf
+++ example/examplea/aws_cloudwatch_log_group.examplea.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "example" {
   name              = var.log_group_name
   kms_key_id        = aws_kms_key.example.arn
-  retention_in_days = 15
+  retention_in_days = 14
 }
