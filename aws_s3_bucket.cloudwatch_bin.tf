5988f9c523a53bc1ec59fd0583d1a9078cd61798
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:26:02 +0100

    fix: skip CKV2_AWS_61 and CKV2_AWS_62 on S3 bucket resources

--- aws_s3_bucket.cloudwatch_bin.tf
+++ aws_s3_bucket.cloudwatch_bin.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket" "cloudwatch_bin" {
+  # checkov:skip=CKV2_AWS_61: Lifecycle configuration not required for this bucket
+  # checkov:skip=CKV2_AWS_62: Event notifications not required for this bucket
   # tfsec:ignore:AWS077
   # checkov:skip=CKV_AWS_144: Not relevant
   # checkov:skip=CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
