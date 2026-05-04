6811dac6aa3444c9567c2e49967048af043df805
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:30:49 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- aws_iam_role_policy.permissionsforfirehose.tf
+++ aws_iam_role_policy.permissionsforfirehose.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "permissionsforfirehose" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   role   = aws_iam_role.firehosetos3.id
   policy = data.aws_iam_policy_document.firehose.json
 }
