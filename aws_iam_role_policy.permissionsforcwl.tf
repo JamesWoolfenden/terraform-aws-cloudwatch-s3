1cdd3ae2a5642acdcf83e0f331530334cc8874e6
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:11:50 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- aws_iam_role_policy.permissionsforcwl.tf
+++ aws_iam_role_policy.permissionsforcwl.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "permissionsforcwl" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   role   = aws_iam_role.cwl.id
--- aws_iam_role_policy.permissionsforfirehose.tf
+++ aws_iam_role_policy.permissionsforfirehose.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "permissionsforfirehose" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   role   = aws_iam_role.firehosetos3.id
